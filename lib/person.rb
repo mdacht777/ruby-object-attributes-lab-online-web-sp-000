4220c8cc6e7f9b16786d9738fb743056848bc1f7
Martin Achtermann <mdacht777@gmail.com>
2020-06-14 15:14:37 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+class Person
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+ 
+  def name
+    @name
+  end
+  def job=(dogs_breed)
+    @job = dogs_breed
+  end
+ 
+  def job
+    @job
+  end
+end
